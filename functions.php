ec9d72002b7230a7beadf3cd887967b2bd0dd178
Tien Tien Truong <tientientruong@live.fr>
2022-09-21 09:05:50 -0400

    Rajout de 404 page

--- functions.php
+++ functions.php
@@ -9,6 +9,9 @@ function cidw_5W5_enqueue(){
     wp_enqueue_style('cidw_5w5_police_google',
     "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap",
     false);
+    wp_enqueue_style('cidw_5w5_police_google',
+    "https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500&display=swap",
+    false);
 }
 
 add_action("wp_enqueue_scripts", "cidw_5W5_enqueue");
@@ -20,7 +23,8 @@ Function cidw_5w5_enregistre_mon_menu() {
         array(
             'navPages' => esc_html__('Navigation des pages', 'cidw_5w5'),
             'ListeCours' => esc_html__('Liste des cours', 'cidw_5w5'),
-        )
+            'ListeProfs' => esc_html__('Liste des profs', 'cidw_5w5'),
+            )
     );
 }
 
