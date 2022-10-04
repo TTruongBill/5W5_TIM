f807ca6187fee2946cf82a17e2f260445d7e957b
Tien Tien Truong <tientientruong@live.fr>
2022-10-04 15:57:06 -0400

    Rajout d'une animation de carrousel pour la page principale

--- javascript/caroussel.js
+++ javascript/caroussel.js
@@ -5,34 +5,38 @@ let boite_carrousel_img = document.querySelector(".boite_carrousel_img");
 
 let index = 0;
     for (const img of galerie_img) {
-      let elmImage = document.createElement('img')
-      img.dataset.index = index
-      elmImage.setAttribute('src', img.getAttribute('src'))
-      boite_carrousel_img.append(elmImage)
+      let elmImage = document.createElement('img');
+      img.dataset.index = index;
+      elmImage.setAttribute('src', img.getAttribute('src'));
+      boite_carrousel_img.append(elmImage);
 
-      let bouton = document.createElement('input')
-      bouton.checked=false
-      bouton.type = "radio"
-      bouton.class = "bouton"
-      bouton.name = "bouton"
-      bouton.dataset.index = index++
-      boite_carrousel_nav.append(bouton)
+      let bouton = document.createElement('input');
+      bouton.checked=false;
+      bouton.type = "radio";
+      bouton.class = "bouton";
+      bouton.name = "bouton";
+      bouton.dataset.index = index++;
+      boite_carrousel_nav.append(bouton);
 
 
       bouton.addEventListener('mousedown', function(e){ 
         e.preventDefault;
-        initialise__carrousel__img()
-        boite_carrousel_img.children[this.dataset.index].classList.add('img-ouvrir')
+        initialise__carrousel__img();
+        boite_carrousel_img.children[this.dataset.index].classList.add('img-ouvrir');
       })
 
     }
     function initialise__carrousel__img(){
-      let collectionImg = document.querySelectorAll('.boite_carrousel_img img')
+      let collectionImg = document.querySelectorAll('.boite_carrousel_img img');
       for (let i=0 ; i<collectionImg.length; i++){
         if(boite_carrousel_nav.children[i].checked == true){ 
-          collectionImg[i].classList.remove('img-ouvrir') 
+          collectionImg[i].classList.remove('img-ouvrir') ;
         }
       }
+    }
+
+    function carrousel_bouge(){
+      let collectionImg = document.querySelectorAll('.boite_carrousel_img img');
     }
   })()
   
