6dec329cacbe2fb8de1915f8d40531798f2621cc
Tien Tien Truong <tientientruong@live.fr>
2022-10-11 16:53:47 -0400

    Finissions du nav mobile et rajout de variables de couleurs

---
+++ javascript/menuBurger.js
@@ -0,0 +1,24 @@
+(function(){
+    let checkboxMenu = document.getElementById("menuNavMobile");
+    let menuBurger = document.getElementsByClassName("menuBurger");
+    let line1 = document.getElementById("line1");
+    let line2 = document.getElementById("line2");
+    let line3 = document.getElementById("line3");
+
+    checkboxMenu.addEventListener("click", burgerAnimation);
+
+    function burgerAnimation(){
+        if(checkboxMenu.checked == true){
+            line1.classList.add("ligne1X");
+            line2.classList.add("ligne2X");
+            line3.classList.add("ligne3X");
+            menuBurger[0].classList.add("circleBackground");
+        }else {
+            line1.classList.remove("ligne1X");
+            line2.classList.remove("ligne2X");
+            line3.classList.remove("ligne3X");
+            menuBurger[0].classList.remove("circleBackground");
+        }
+    }
+
+})()
