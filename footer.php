59b789700ba6b431f15b41669274291e5374496d
Tien Tien Truong <tientientruong@live.fr>
2022-10-23 00:36:28 -0400

    Début de l'implémentation du footer + les composantes des cours

--- footer.php
+++ footer.php
@@ -1,6 +1,25 @@
 <footer class="site_footer">
     <section class="site_footer_info">
-        <p>TIM 2022</p>
+        <section class="adresse footer_section">
+            <h4>ADRESSE</h4>
+            <h5>3800 Rue Sherbrooke Est</h5>
+            <h5>Montréal</h5>
+            <h5>QC H1X 2A2</h5>
+        </section>
+        <section class="joindre footer_section">
+            <h4>NOUS JOINDRE</h4>
+            <h5>Trouver une personne/un service</h5>
+            <h5>Trouver un campus de formation</h5>
+            <h5>FAQ</h5>
+        </section>
+        <section class="media footer_section">
+            <h4>SUIVEZ-NOUS</h4>
+            <section class="media_social_liste">
+                <p class="media_social">fb</p>
+                <p class="media_social">ig</p>
+                <p class="media_social">in</p>
+            </section>
+        </section>
     </section>
 </footer>		
 <?php wp_footer(); ?>
--- page-cours.php
+++ page-cours.php
@@ -1,13 +1,26 @@
 <?php get_header(); ?>
-    <main class="site_main_front_page">
-        <?php wp_nav_menu(array('menu'=>"ListeCours",
+    <main class="site_main_cours_page">
+        <h3>Cours</h3>
+        <section class="left-right">
+            <h2><</h2>
+            <h1>SESSION 1</h1>
+            <h2>></h2>
+        </section>
+        <section class="cours">
+            <p>aadawd</p>
+        <section>
+                <h1>Titre du cours</h1>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt doloremque eligendi porro possimus quibusdam magnam, consequuntur sed corrupti culpa illum consequatur nemo veritatis ut iusto illo unde repudiandae aliquid numquam!</p>
+            </section>
+        </section>
+        <?php /*wp_nav_menu(array('menu'=>"ListeCours",
                                 "container"=>"nav"));
         ?>
         <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
         <?php the_post_thumbnail("thumbnail"); ?>
         <?php the_content();?> 
         <?php endwhile;?>
-        <?php endif;?>
+        <?php endif;*/?>
         
     </main>
 <?php get_footer(); ?>
