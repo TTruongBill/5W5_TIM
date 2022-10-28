fbd33579eafd86e1f0e2ec5e9cd384b3b58965c9
Tien Tien Bill Truong <tientientruong1@gmail.com>
2022-10-27 21:51:08 -0400

    Rajout de commentaire dans le js activeNav

--- javascript/activeNav.js
+++ javascript/activeNav.js
@@ -1,6 +1,8 @@
-let lis = document.querySelectorAll("#menu-navpages-1 li");
-let locationName = "/wordpress/index.php" ;
+let lis = document.querySelectorAll("#menu-navpages-1 li");//var qui contient la liste de navmenu
+let locationName = "/wordpress/index.php"; //Pour la base de donnée locale, sinon, enleve sur siteground
 
+//Sur la base de donnée locale, rajouter celui ci
+//Si la location du site est égale au nom de la page, rajouter la classe qui permet de rajouter une couleur sur le nav précis
 if(location.pathname==(locationName + "/cours/")) {
     lis[0].classList.add("navCouleur");
 } else if (location.pathname==(locationName + "/prof/")){
@@ -14,6 +16,24 @@ if(location.pathname==(locationName + "/cours/")) {
 } else if (location.pathname==(locationName + "/inscription/")){
     lis[5].classList.add("navCouleur");
 }
- 
+
+
+//Sur siteground, afficher celui ci
+/*
+if(location.pathname==("/cours/")) {
+    lis[0].classList.add("navCouleur");
+} else if (location.pathname==("/prof/")){
+    lis[1].classList.add("navCouleur");
+} else if (location.pathname==("/projet-etudiant/")){
+    lis[2].classList.add("navCouleur");
+} else if (location.pathname==("/vie-etudiante/")){
+    lis[3].classList.add("navCouleur");
+} else if (location.pathname==("/avenir/")){
+    lis[4].classList.add("navCouleur");
+} else if (location.pathname==("/inscription/")){
+    lis[5].classList.add("navCouleur");
+}
+*/
+
 
 
