860cf78234b57ae01d2fd3befca294cb52e08593
Tien Tien Bill Truong <tientientruong1@gmail.com>
2022-10-27 21:43:17 -0400

    Rajout de commentaires pour le carrousel de prof

--- javascript/carrouselProf.js
+++ javascript/carrouselProf.js
@@ -1,59 +1,71 @@
-let carrousel_left_right = document.getElementById("left-right");
-let arrows = document.querySelectorAll(".left-right button");
-let enseignants = document.getElementsByClassName("enseignants");
-let listeEnseignant = document.getElementById("listeProfs");
+let arrows = document.querySelectorAll(".left-right button");//var contenant les flèches gauche droite des carrousels
+let enseignants = document.getElementsByClassName("enseignants");//var contenant la liste des enseignants(un par un) - 14 objets(profs)
+let listeEnseignant = document.getElementById("listeProfs");//var contenant la boite avec tous les enseignants - (1 object)
 
-let nbProf = 0;
+let nbProf = 0;//nb pour incrémenter le prof dans lequel on est rendu
 
-keepTeachs();
-listeEnseignant.classList.add("rajoutLeft");
+//Initialisation de la page rajouter 2 profs
+enleverProf();
 enseignants[nbProf].style.display = "flex";
 enseignants[nbProf + 1].style.display = "flex";
 
-enableDisableArrows();
+//rajouter la classe qui rajoute un style left
+listeEnseignant.classList.add("rajoutLeft");
 
+enableDisableArrows();//désactiver la flèche de gauche
+
+//Lorsque la flèche de gauche est clicker,
 arrows[0].addEventListener("click", () => {
-    listeEnseignant.classList.remove("rajoutRight");
+    listeEnseignant.classList.remove("rajoutRight"); //Enlever la classe qui rajoute un style right
+    //Si le nbProf est en haut de 0 et qu'il n'est pas égal à 1, 
+    //rajouter 3 prof à partir de l'incrémentation et ensuite décrémenter
     if(nbProf > 0 && nbProf != 1){
-        //enseignants[nbProf - 1].className.add();
-        //enseignants[nbProf].className.remove();
-        keepTeachs();
+        enleverProf();
         enseignants[nbProf].style.display = "flex";
         enseignants[nbProf - 1].style.display = "flex";
         enseignants[nbProf - 2].style.display = "flex";
         nbProf--;
-    } else if(nbProf == 1){
-        keepTeachs();
+    } else if(nbProf == 1){//Sinon si nbProf est égal à 1
+        //rajouter 2 prof à partir de l'incrémentation, rajouter la classe qui rajoute un style left,
+        //ensuite décrémenter
+        enleverProf();
         enseignants[nbProf].style.display = "flex";
         enseignants[nbProf - 1].style.display = "flex";
         listeEnseignant.classList.add("rajoutLeft");
         nbProf--;
     }
+    //désactiver ou activer la flèche de gauche
     enableDisableArrows();
 })
 
 arrows[1].addEventListener("click", () => {
-    listeEnseignant.classList.remove("rajoutLeft");
+    listeEnseignant.classList.remove("rajoutLeft");//Enlever la classe qui rajoute un style left
+    //Si le nbProf est en bas de 13 et qu'il n'est pas égal à 12, 
+    //rajouter 3 prof à partir de l'incrémentation et ensuite incrémenter
     if(nbProf < 13 && nbProf != 12){
-       // enseignants[nbProf + 1].className.add();
-        //enseignants[nbProf].className.remove();
-        keepTeachs();
+        enleverProf();
         enseignants[nbProf].style.display = "flex";
         enseignants[nbProf + 1].style.display = "flex";
         enseignants[nbProf + 2].style.display = "flex";
         nbProf++;
-    }else if(nbProf == 12){
-        keepTeachs();
+    }else if(nbProf == 12){//Sinon si nbProf est égal à 12
+        //rajouter 2 prof à partir de l'incrémentation, rajouter la classe qui rajoute un style right,
+        //ensuite incrémenter
+        enleverProf();
         enseignants[nbProf].style.display = "flex";
         enseignants[nbProf + 1].style.display = "flex";
         listeEnseignant.classList.add("rajoutRight");
         nbProf++;
     }
+    //désactiver ou activer la flèche de droite
     enableDisableArrows();
 })
 
-//Fonction qui permet d'active ou enlever l'animation de hover su 
+//Fonction qui permet d'activer ou désactiver les boutons pour changer de profs
 function enableDisableArrows(){
+    //Si nbProf est égal à 1, la flèche de gauche est désactivé
+    //Sinon si nbprof est en haut de 0 et en bas de 13, activer les 2 flèches
+    //sinon si nbProf est égal à 13, la flèche de droite est désactivé
     if(nbProf == 0){
         arrows[0].disabled = true;
     }else if(nbProf > 0 && nbProf < 13){
@@ -64,8 +76,11 @@ function enableDisableArrows(){
     }
 }
 
-function keepTeachs(){
-    if(nbProf => 1 && nbProf <= 13){
+//Fonction qui permet d'enlever l'affichage de tous les profs
+function enleverProf(){
+    //Si nbProf est en haut de 0 et en bas de 14
+    //rajouter un display none à tous les éléments du array de profs
+    if(nbProf > 0 && nbProf < 14){
         for(let i = 0; i < enseignants.length; i++){
             enseignants[i].style.display= "none";
         }
