493b102bccfe243161adcf439e41a25a801345f8
Tien Tien Truong <tientientruong@live.fr>
2022-10-31 11:46:23 -0400

    Pauffinage du code Js dans carrouselProf et rajout de format desktop  pour la page des profs (la forme basique))

--- page-prof.php
+++ page-prof.php
@@ -1,24 +1,37 @@
-<?php get_header(); ?>
+<?php 
+    $titre = get_the_title();
+    get_header(); 
+?>
     <main class="site_main_prof_page">
-        <h3><?= get_the_title(); ?></h3>
-        <section class="left-right">
-            <button type="button"><</button>
-            <h1>LES PROFS DU TIM</h1>
-            <button type="button">></button>
+        <?php 
+            $catPost = get_posts(array('category_name' => 'enseignants', "numberposts" => 14));
+            foreach ($catPost as $post) : setup_postdata($post); 
+        ?> 
+        <section id="imgProf">
+            <?php the_post_thumbnail(); ?>
         </section>
-        <section id="listeProfs">
-             <?php 
+        <?php endforeach;?>
+        <section id="right">
+            <h3><?= $titre; ?></h3>
+            <section class="left-right">
+                <button type="button"><</button>
+                <h1>LES PROFS DU TIM</h1>
+                <button type="button">></button>
+            </section>
+            <section id="listeProfs">
+                <?php 
                 $catPost = get_posts(array('category_name' => 'enseignants', "numberposts" => 14));
-                foreach ($catPost as $post) : setup_postdata($post); ?> 
-                <section class='enseignants'> 
-                    <section>
-                        <h1><?php the_title(); ?></h1>
-                        <?php the_post_thumbnail(); ?>
-                        <p><?php the_excerpt();?></p>
+                foreach ($catPost as $post) : setup_postdata($post); 
+                ?> 
+                    <section class='enseignants'> 
+                        <section>
+                            <h1><?php the_title(); ?></h1>
+                            <?php the_post_thumbnail(); ?>
+                            <p><?php the_excerpt();?></p>
+                        </section>
                     </section>
-                </section>
-                <?php endforeach;?>
+        <?php endforeach;?>
+            </section>
         </section>
-       
     </main>
 <?php get_footer(); ?>
