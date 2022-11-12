0f037fdbe0506f0a35639b35a3802e3cb71e470f
Tien Tien Truong <tientientruong@live.fr>
2022-11-12 14:05:43 -0500

    Delete lirePlusCours, fusionner avec carrouselSession, Data-order fonction ne maintenant dans la page cours

--- javascript/carrouselSession.js
+++ javascript/carrouselSession.js
@@ -2,14 +2,21 @@ let titreSession = document.getElementById("sessions");//var qui contient le tit
 let arrows = document.querySelectorAll("#left-right button");//var contenant les flèches gauche droite des carrousels
 let nbSession = 0;//nb pour incrémenter la session dans laquelle on est rendu
 let sessionsListe = document.getElementsByClassName("session");//var contenant la liste des session(un par un) - 6 objets(session)
-let mediaQuery = window.matchMedia("(min-width: 600px)");
+let counter = 0;
+
+let chiffreCours;
+let ouvrirCours;
+let coursSession;
+let fermerCours;
+
+ ouvrirCoursButton();
 
 //Initialisation de la page afficher session 1
 sessionsListe[0].style.display = "flex";
 enableDisableArrows();//désactiver la flèche de gauche
 
 //Lorsque la flèche de gauche est clicker,
-arrows[0].addEventListener("click", () => {
+arrows[0].addEventListener("mousedown", () => {
     //Si nbSession est en haut de 0 
     if(nbSession > 0){
         sessionsListe[nbSession].style.display = "none";//rajouter un display none sur la session actuelle
@@ -17,10 +24,12 @@ arrows[0].addEventListener("click", () => {
         titreSession.innerHTML = "SESSION " + (nbSession + 1);//changer le titre pour la prochaine session
         sessionsListe[nbSession].style.display = "flex";
     }
+    ouvrirCoursButton();
     enableDisableArrows();
 })
 //Lorsque la flèche de droite est clicker,
-arrows[1].addEventListener("click", () => {
+arrows[1].addEventListener("mousedown", () => {
+   
     //Si nbSession est en bas de 5
     if(nbSession < 5){
         sessionsListe[nbSession].style.display = "none";//rajouter un display none sur la session actuelle
@@ -28,7 +37,7 @@ arrows[1].addEventListener("click", () => {
         titreSession.innerHTML = "SESSION " + (nbSession + 1);//changer le titre pour la prochaine session
         sessionsListe[nbSession].style.display = "flex";
     }
-    
+    ouvrirCoursButton();
     enableDisableArrows();
 })
 
@@ -49,4 +58,45 @@ function enableDisableArrows(){
     } else if(nbSession == 5){
         arrows[1].disabled = true;
     }
+}
+
+function ouvrirCoursButton(){
+    ouvrirCours = document.querySelectorAll("#session" + (nbSession + 1) + " > .cours > p > .cours_ouvrir");
+    coursSession = document.querySelectorAll("#session" + (nbSession + 1) + " > .cours");
+    fermerCours = document.querySelectorAll("#session" + (nbSession + 1) + " > .cours > .cours_fermer");
+
+    for(let i = 0; i <ouvrirCours.length; i++){
+        ouvrirCours[i].addEventListener("mousedown", () => {
+            for(let j = 0; j < ouvrirCours.length; j++){
+                fermerCoursAuto(coursSession[j], fermerCours[j], ouvrirCours[j]);
+    
+            }
+            fermerCours[i].style.display = "block";
+            ouvrirCours[i].style.display = "none";
+            chiffreCours = i;
+            coursSession[i].setAttribute("data-order", 1);
+            
+            for(let j = chiffreCours - 1; j >= 0;j--){
+                coursSession[j].setAttribute("data-order", j + 2); 
+            }
+            for(let j = chiffreCours + 1; j < coursSession.length; j++){
+                coursSession[j].setAttribute("data-order", j + 1);
+            }
+    
+        })
+        
+        fermerCours[i].addEventListener("mousedown", () => {
+            fermerCoursAuto(coursSession[i], fermerCours[i], ouvrirCours[i])
+        })
+    
+    }
+    
+}
+
+
+function fermerCoursAuto(cours, fermer, ouvrir){
+    cours.classList.remove("lirePlusCours");
+    fermer.style.display = "none";
+    ouvrir.style.display = "block";
+
 }
