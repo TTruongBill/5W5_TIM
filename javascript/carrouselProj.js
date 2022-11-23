485f033bfc321cd51745adb4d36e5f6ff6f1c00f
Tien Tien Truong <tientientruong@live.fr>
2022-11-23 15:44:56 -0500

    template pour prof/proj et adaptation du javascript

--- javascript/carrouselProj.js
+++ javascript/carrouselProj.js
@@ -1,98 +1,98 @@
 let arrows = document.querySelectorAll(".left-right button");//var contenant les flèches gauche droite des carrousels
-let enseignants = document.getElementsByClassName("enseignants");//var contenant la liste des enseignants(un par un) - 14 objets(profs)
-let nbRandom = Math.floor(Math.random() * enseignants.length);//nombre aléatoire pour générer un prof différent à chaque refresh(pour format desktop)
+let contenu = document.getElementsByClassName("contenuComplet");//var contenant la liste des contenu(un par un) - 14 objets(profs)
+let randomNum = Math.floor(Math.random() * contenu.length);//nombre aléatoire pour générer un prof différent à chaque refresh(pour format desktop)
 let counter = document.getElementsByClassName("counter");//Var contenant le compteur du nombre de prof
-let imgCarrousel = document.getElementsByClassName("imgProf");//var contenant la liste des images des enseignants(un par un) - 14 objets(images)
+let imgCarrousel = document.getElementsByClassName("imgThumbnail");//var contenant la liste des images des contenu(un par un) - 14 objets(images)
 
-let nbProf = nbRandom;
+let nbRandom = randomNum;
 //***************Initialisation de la page **********************
 enleverProf();//Enlever tous les profs
 
-if(nbProf == 10){
-    imgCarrousel[nbProf - 1].style.display = "flex";
-    imgCarrousel[nbProf].style.display = "flex";
+if(nbRandom == contenu.length - 1){
+    imgCarrousel[nbRandom - 1].style.display = "flex";
+    imgCarrousel[nbRandom].style.display = "flex";
     imgCarrousel[0].style.display = "flex";
     imgCarrousel[0].style.order = "1";
-} else if(nbProf == 0){
-    imgCarrousel[nbProf].style.display = "flex";
-    imgCarrousel[nbProf + 1].style.display = "flex";
-    imgCarrousel[enseignants.length - 1].style.display = "flex";
-    imgCarrousel[enseignants.length - 1].style.order = "-1";
+} else if(nbRandom == 0){
+    imgCarrousel[nbRandom].style.display = "flex";
+    imgCarrousel[nbRandom + 1].style.display = "flex";
+    imgCarrousel[contenu.length - 1].style.display = "flex";
+    imgCarrousel[contenu.length - 1].style.order = "-1";
 
 }else{
-    imgCarrousel[nbProf].style.display = "flex";
-    imgCarrousel[nbProf + 1].style.display = "flex";
-    imgCarrousel[nbProf - 1].style.display = "flex";
+    imgCarrousel[nbRandom].style.display = "flex";
+    imgCarrousel[nbRandom + 1].style.display = "flex";
+    imgCarrousel[nbRandom - 1].style.display = "flex";
 }
 
-enseignants[nbProf].style.display = "flex";//Afficher le prof selon le prof choisi
+contenu[nbRandom].style.display = "flex";//Afficher le prof selon le prof choisi
 for(let i = 0; i < counter.length; i++){
-    counter[i].innerHTML = nbProf + 1 + "/" + enseignants.length;//Écrire dans le compteur le prof auquel on est rendu et le nombre de prof total
+    counter[i].innerHTML = nbRandom + 1 + "/" + contenu.length;//Écrire dans le compteur le prof auquel on est rendu et le nombre de prof total
 }
 //Lorsque la flèche de gauche est clicker,
 arrows[0].addEventListener("mousedown", () => {
     enleverProf();//enlever tous les profs
-    imgCarrousel[nbProf].style.display = "flex";
+    imgCarrousel[nbRandom].style.display = "flex";
 
-    if(nbProf > 0 && nbProf != 1){//si nbProf est plus grand que 0 et plus petit ou egal à 14
-        nbProf--;//Décrémenter
-        imgCarrousel[nbProf - 1].style.display = "flex";
+    if(nbRandom > 0 && nbRandom != 1){//si nbRandom est plus grand que 0 et plus petit ou egal à 14
+        nbRandom--;//Décrémenter
+        imgCarrousel[nbRandom - 1].style.display = "flex";
 
-    }else if(nbProf == 0){//sinon si nbProf est égal à 0
-        nbProf = 10;//mettre nbProf à 13
-        imgCarrousel[nbProf].style.order = "-1";
-        imgCarrousel[nbProf - 1].style.display = "flex";
-        imgCarrousel[nbProf - 1].style.order = "-1";
+    }else if(nbRandom == 0){//sinon si nbRandom est égal à 0
+        nbRandom = contenu.length - 1;//mettre nbRandom à 13
+        imgCarrousel[nbRandom].style.order = "-1";
+        imgCarrousel[nbRandom - 1].style.display = "flex";
+        imgCarrousel[nbRandom - 1].style.order = "-1";
 
-    }else if(nbProf == 1){//si nbProf est égal à 13, afficher le prof suivant
-        nbProf--;//Décrémenter
-        imgCarrousel[10].style.display = "flex";
-        imgCarrousel[10].style.order = "-1";
+    }else if(nbRandom == 1){//si nbRandom est égal à 13, afficher le prof suivant
+        nbRandom--;//Décrémenter
+        imgCarrousel[contenu.length - 1].style.display = "flex";
+        imgCarrousel[contenu.length - 1].style.order = "-1";
     }
 
-    imgCarrousel[nbProf].style.display = "flex";
-    enseignants[nbProf].style.display = "flex";//mettre en flex le display du prof actuel
+    imgCarrousel[nbRandom].style.display = "flex";
+    contenu[nbRandom].style.display = "flex";//mettre en flex le display du prof actuel
     for(let i = 0; i < counter.length; i++){
-        counter[i].innerHTML = nbProf + 1 + "/" + enseignants.length;//Écrire dans le compteur le prof auquel on est rendu et le nombre de prof total
+        counter[i].innerHTML = nbRandom + 1 + "/" + contenu.length;//Écrire dans le compteur le prof auquel on est rendu et le nombre de prof total
     }
 })
 //Lorsque la flèche de droite est clicker,
 arrows[1].addEventListener("mousedown", () => {
     enleverProf();//enlever tous les profs
 
-    if(nbProf < 10 && nbProf != 9 ) {//si nbProf est plus grand ou egal que 0 et plus petit que 13
-        imgCarrousel[nbProf].style.display = "flex";
-        nbProf++;//Incrémenter
-        imgCarrousel[nbProf].style.display = "flex";
-        imgCarrousel[nbProf + 1].style.display = "flex";
+    if(nbRandom < contenu.length - 1 && nbRandom != contenu.length - 2 ) {//si nbRandom est plus grand ou egal que 0 et plus petit que 13
+        imgCarrousel[nbRandom].style.display = "flex";
+        nbRandom++;//Incrémenter
+        imgCarrousel[nbRandom].style.display = "flex";
+        imgCarrousel[nbRandom + 1].style.display = "flex";
 
-    }else if(nbProf == 9){//sinon si nbProf est égal à 13
-        imgCarrousel[nbProf].style.display = "flex";
-        nbProf++;//Décrémenter
-        imgCarrousel[nbProf].style.display = "flex";
+    }else if(nbRandom == contenu.length - 2){//sinon si nbRandom est égal à 13
+        imgCarrousel[nbRandom].style.display = "flex";
+        nbRandom++;//Décrémenter
+        imgCarrousel[nbRandom].style.display = "flex";
         imgCarrousel[0].style.display = "flex";
         imgCarrousel[0].style.order = "1";
-    }else if(nbProf == 10){//sinon si nbProf est égal à 13
-        imgCarrousel[nbProf].style.display = "flex";
-        imgCarrousel[nbProf].style.order = "-1";
-        nbProf = 0;//mettre nbProf à 13
-        imgCarrousel[nbProf].style.display = "flex";
-        imgCarrousel[nbProf + 1].style.display = "flex";
+    }else if(nbRandom == contenu.length - 1){//sinon si nbRandom est égal à 13
+        imgCarrousel[nbRandom].style.display = "flex";
+        imgCarrousel[nbRandom].style.order = "-1";
+        nbRandom = 0;//mettre nbRandom à 13
+        imgCarrousel[nbRandom].style.display = "flex";
+        imgCarrousel[nbRandom + 1].style.display = "flex";
     }
 
-    enseignants[nbProf].style.display = "flex"; //mettre en flex le display du prof actuel
+    contenu[nbRandom].style.display = "flex"; //mettre en flex le display du prof actuel
     for(let i = 0; i < counter.length; i++){
-        counter[i].innerHTML = nbProf + 1 + "/" + enseignants.length;//Écrire dans le compteur le prof auquel on est rendu et le nombre de prof total
+        counter[i].innerHTML = nbRandom + 1 + "/" + contenu.length;//Écrire dans le compteur le prof auquel on est rendu et le nombre de prof total
     }
 })
 
 //Fonction qui permet d'enlever l'affichage de tous les profs
 function enleverProf(){
-    //Si nbProf est en haut de 0 et en bas de 14
+    //Si nbRandom est en haut de 0 et en bas de 14
     //rajouter un display none à tous les éléments du array de profs
-    if(nbProf => 0 && nbProf < 11){
-        for(let i = 0; i < enseignants.length; i++){
-            enseignants[i].style.display= "none";
+    if(nbRandom => 0 && nbRandom < contenu.length){
+        for(let i = 0; i < contenu.length; i++){
+            contenu[i].style.display= "none";
             imgCarrousel[i].style.display = "none";
             imgCarrousel[i].style.order = "0";
         }
